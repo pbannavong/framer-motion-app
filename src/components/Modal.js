5233f8d7fcb85409cb2ae02b6bd620ec3ff3c317
pbannavong <Phillip.Bannavong@gmail.com>
2020-09-13 21:17:00 -0700

    fixed popup window exit bug with modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -3,11 +3,19 @@ import { Link } from 'react-router-dom'
 import { motion, AnimatePresence } from 'framer-motion'
 
 const backdrop = {
-  visible: {
-    opacity: 1
-  },
+  visible: { opacity: 1 },
+  hidden: { opacity: 0 }
+}
+
+const modal = {
   hidden: {
+    y: "-100vh",
     opacity: 0
+  },
+  visible: {
+    y: "200px",
+    opacity: 1,
+    transition: { delay: 0.5 }
   }
 }
 
@@ -19,8 +27,16 @@ const Modal = ({ showModal, setShowModal }) => {
           variants={backdrop}
           animate="visible"
           initial="hidden"
+          exit="hidden"
         >
-
+          <motion.div className="modal"
+            variants={modal}
+          >
+            <p>Want to make another pizza?</p>
+            <Link to="/">
+              <button>Start Again</button>
+            </Link>
+          </motion.div>
         </motion.div>
       )}
     </AnimatePresence>
