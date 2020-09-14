d48e4821fde4ce7c02bb12a00c860a4bc2ced212
pbannavong <Phillip.Bannavong@gmail.com>
2020-09-13 20:16:13 -0700

    added button variants

--- src/components/Toppings.js
+++ src/components/Toppings.js
@@ -30,6 +30,18 @@ const nextVariants = {
   }
 }
 
+const buttonVariants = {
+  hover: {
+    scale: 1.1,
+    textShadow: "0px 0px 8px rgb(255,255,255)",
+    boxShadow: "0px 0px 8px rgb(255,255,255)",
+    transition: {
+      duration: 0.3,
+      yoyo: Infinity
+    }
+  }
+}
+
 const Toppings = ({ addTopping, pizza }) => {
   let toppings = ['mushrooms', 'peppers', 'onions', 'olives', 'extra cheese', 'tomatoes'];
 
@@ -57,10 +69,8 @@ const Toppings = ({ addTopping, pizza }) => {
 
       <Link to="/order">
         <motion.button
-          whileHover={{
-            scale: 1.1,
-            textShadow: "0px 0px 8px rgb(255,255,255)"
-          }}
+          variants={buttonVariants}
+          whileHover="hover"
         >
           Order
         </motion.button>
